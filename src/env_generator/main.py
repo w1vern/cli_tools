a13b959abf28e204d4d56a314b122990d0e325da
w1vern <bndvvl@gmail.com>
2025-06-10 05:11:56 +0300

    fix env-generator first equal symbol

--- src/env_generator/main.py
+++ src/env_generator/main.py
@@ -8,16 +8,19 @@ def env_generator(args: Args, out: TextIO) -> int:
     files = get_repo_files(args, [".env.example"])
     env_dict: dict[str, str] = dict()
 
-    def parse_env_line(line: str) -> tuple[str, str]:
+    def parse_env_line(line: str) -> tuple[str, str] | None:
         if "=" not in line or line.strip().startswith("#"):
-            return "", ""
+            return None
         key, value = line.replace(" ", "").replace("\t", "").split("=", 1)
         return key.strip(), value.strip()
 
     for file in files:
         with open(args.root_dir / file, "r", encoding="utf-8", errors="replace") as f:
             for line in f:
-                key, value = parse_env_line(line)
+                parsed = parse_env_line(line)
+                if not parsed:
+                    continue
+                key, value = parsed
                 if key in env_dict and value not in env_dict[key].split("|"):
                     env_dict[key] += f"|{value}"
                 else:
